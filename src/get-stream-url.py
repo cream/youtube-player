ec0168ca7ff09a1f27e1bdd8b5bffe2565d8d20a
Jonas Haag <jonas@lophus.org>
2010-08-02 19:03:42 +0200

    Fix get-stream-url + autoplay option

--- src/get-stream-url.py
+++ src/get-stream-url.py
@@ -11,6 +11,8 @@ video.request_video_info()
 
 try:
     resolution = sys.argv[2]
+    if resolution == '-':
+        raise IndexError
 except IndexError:
     print video.stream_urls.keys()
     resolution = video.stream_urls.iterkeys().next()
@@ -22,13 +24,23 @@ else:
         'mid' : '720p',
         'high' : '1080p',
         '360' : '360p',
+        '480' : '480p',
+        '480+' : '480p+',
         '720' : '720p',
         '1080' : '1080p'
     }.get(resolution, resolution)
     if resolution not in video.stream_urls:
         print "Warning: Requested video {video_id} is not available in {resolution}!".format(**globals())
-        resolution = video.resolutions[0]
+        resolution = video.stream_urls.iterkeys().next()
 
 video_url = video.stream_urls[resolution]
+
+try:
+    program = sys.argv[3]
+except IndexError:
+    pass
+else:
+    import os
+    os.system("%s '%s'" % (program, video_url))
 
 print "mp4 URL for {video_id} ({resolution}) is\n{video_url}".format(**globals())
