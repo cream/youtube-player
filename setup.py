63478b231aa4519634ef5ad878390a3a8d7a78b3
Sebastian Billaudelle <stein@cream-project.org>
2010-10-29 17:53:37 +0200

    Changed setup script.

--- setup.py
+++ setup.py
@@ -1,14 +1,8 @@
 from cream.dist import setup
 
-class NotImplemented(BaseException):
-    pass
-
-raise NotImplemented
-
-"""
 setup('src/manifest.xml',
     data_files = [
-        ('{module_dir}', ['src/hotkey.py', 'src/manifest.xml'])
+        ('{module_dir}/interface', ['src/interface/youtube-player.svg', 'src/interface/interface.ui']),
+        ('{module_dir}', ['src/common.py', 'src/get-stream-url.py', 'src/youtube-player.py', 'src/youtube.py', 'src/buffer.py', 'src/throbberwidget.py', 'src/manifest.xml', 'src/configuration.xml']),
         ]
     )
-"""
